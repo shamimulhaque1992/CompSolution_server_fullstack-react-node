d39450a2f12743986704f62deff3c7cbe230adff
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2022-05-22 11:18:07 +0600

    packages added

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send("This is Comp-Solution server running")
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hqx62.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-app.listen(port, ()=>{
-    console.log(`Lesting to port ${port}`);
-})
+async function run() {
+  try {
+    await client.connect();
+    const toolsCollections = client.db("comp-solution").collection("tools");
+
+    app.get("/tools", async(req, res) => {
+      const query = {};
+      const cursor = toolsCollections.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("This is Comp-Solution server running");
+});
+
+app.listen(port, () => {
+  console.log(`Lesting to port ${port}`);
+});
