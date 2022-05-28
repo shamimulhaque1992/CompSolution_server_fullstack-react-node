3101ef802548123afa7e5ccfd8df2d51909fe48a
shamimulhaque162 <khandoker15-1992@diu.edu.bd>
2022-05-28 13:19:43 +0600

    dsfd

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-app.use(cors());
+app.use(cors(
+  origin:{"http://localhost:3000",
+  "https://serene-shelf-91638.herokuapp.com"}
+));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hqx62.mongodb.net/?retryWrites=true&w=majority`;
